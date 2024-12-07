cc8414440a976c674a222a4d7b71537542c76f3d
Hexploit <hexploitUi@gmail.com>
2024-12-07 17:07:30 +0100

    Update Knight Animations.lua

--- Knight Animations.lua
+++ Knight Animations.lua
@@ -10,13 +10,13 @@ local function notifyUser(title, text)
 end
 
 local function applyKnightAnimations(animate)
-  animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=657595757"
-  animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=657568135"
-  animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=657552124"
-  animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=657564596"
-  animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=658409194"
-  animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=658360781"
-  animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=657600338"
+    animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=657595757"
+    animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=657568135"
+    animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=657552124"
+    animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=657564596"
+    animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=658409194"
+    animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=658360781"
+    animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=657600338"
 end
 
 local function resetToDefaultAnimations(animate)
@@ -25,21 +25,21 @@ local function resetToDefaultAnimations(animate)
     animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=2510202577"
     animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=2510198475"
     animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=2510197830"
-    animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=2510192778"
     animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=2510195892"
+    animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=2510192778"
 end
 
 local function toggleAnimations()
     local animate = character:WaitForChild("Animate")
     
     if _G.animKnightActive then
-        print("Knight Animations Loaded")
+        print("Applying Knight animations...")
         applyKnightAnimations(animate)
-        notifyUser("🎭  Hexploit 🎭")
+        notifyUser("Knight Animations Loaded", "🎭 Hexploit 🎭")
     else
-        print("Reset Animations To R15")
+        print("Resetting animations to default...")
         resetToDefaultAnimations(animate)
-        notifyUser("🎭  Hexploit 🎭")
+        notifyUser("Reset Animations To R15", "🎭  Hexploit 🎭")
     end
 end
 
